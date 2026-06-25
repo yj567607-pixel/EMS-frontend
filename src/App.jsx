8989ffc4a9d9905fad8f122f271f07adeed247eb
Indra Kumar Yadav <yj567607@gmail.com>
2026-06-25 11:45:16 +0530

    Update API_URL to production endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     salary: "",
   });
 
-  const API_URL = "http://localhost:5000/employees";
+  const API_URL  = "https://ems-backend-7jpw.onrender.com/employees";
 
   const getEmployees = async () => {
     const params = new URLSearchParams();
